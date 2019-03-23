7640e992e6ee35a355c4a553247dcba8832201fe
sineond <sineond@gmail.com>
2019-03-23 22:10:05 +0300

    Много обновлений

--- web_app.py
+++ web_app.py
@@ -29,30 +29,22 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-
-
-@app_child.route('/', methods=['GET', 'POST'])
+@app_child.route('/upload_file', methods=['GET', 'POST'])
 
 def upload_file():
-    conn = sqlite3.connect('child.sqlite')
-    conn.row_factory = dict_factory
-    c = conn.cursor()
     if request.method == 'POST':
-
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-
             file.save(os.path.join(app_child.config['UPLOAD_FOLDER'], filename))
-            return redirect('/add_book')
-
+            conn = sqlite3.connect('child.sqlite')
+            conn.row_factory = dict_factory
+            c = conn.cursor()
+            return redirect('/add_user')
     return render_template('upload_file.html')
 
 
-
-
-@app_child.route('/add_book', methods=['GET','POST'])
+@app_child.route('/', methods=['GET','POST'])
 def add_book():
     if request.method == 'POST':
         meta_info = {}
@@ -66,6 +58,7 @@ def add_book():
         meta_info['kid'] = request.form.get('kid')
         meta_info['junior'] = request.form.get('junior')
         meta_info['youth'] = request.form.get('youth')
+        meta_info['age_category'] = request.form.get('age_category')
         meta_info_a = {}
         meta_info_a['author_id'] = request.form.get('author_id')
         meta_info_a['last'] = request.form.get('last')
@@ -78,6 +71,10 @@ def add_book():
         meta_edit['author_name'] = request.form.get('author_name')
         meta_edit['title'] = request.form.get('title')
         meta_edit['book_id'] = request.form.get('book_id')
+        meta_pseud = {}
+        meta_pseud['last_p'] = request.form.get('last_p')
+        meta_pseud['first_p'] = request.form.get('first_p')
+        meta_pseud['middle_p'] = request.form.get('middle_p')
 
         conn = sqlite3.connect('child.sqlite')
         c = conn.cursor()
@@ -86,22 +83,58 @@ def add_book():
                   "VALUES "
                   "('{booktitle}', '{year}', '{city}', '{publisher}', '{publisher_unified}', '{printrun}', '{kid}', '{junior}', '{youth}' )"
                   "".format(**meta_info))
-        c.execute("INSERT INTO meta_authors "
-                  "('last', 'first', 'middle', 'sex', 'birth_year', 'death_year')"
-                  "VALUES "
-                  "('{last}', '{first}', '{middle}', '{sex}', '{birth_year}', '{death_year}')"
-                  "".format(**meta_info_a))
         c.execute("INSERT INTO meta_editions "
                   "('author_name', 'title', 'book_id')"
                   "VALUES "
                   "('{author_name}', '{title}', (SELECT last_insert_rowid()))"
                   "".format(**meta_edit))
+        c.execute("INSERT INTO meta_authors "
+                  "('last', 'first', 'middle', 'sex', 'birth_year', 'death_year')"
+                  "VALUES "
+                  "('{last}', '{first}', '{middle}', '{sex}', '{birth_year}', '{death_year}')"
+                  "".format(**meta_info_a))
+        c.execute("INSERT INTO meta_pseudo "
+                  "('last_p', 'first_p', 'middle_p', 'author_id')"
+                  "VALUES "
+                  "('{last_p}', '{first_p}', '{middle_p}', (SELECT last_insert_rowid()))"
+                  "".format(**meta_pseud))
 
         conn.commit()
-
         conn.close()
-        return redirect("/Success")
+        return redirect('/upload_file')
     return render_template('add_book.html')
+
+
+
+
+@app_child.route('/add_user', methods=['GET','POST'])
+def add_user():
+    user_created = False
+    error_message = ""
+    if request.method == 'POST':
+        meta_user = {}
+        meta_user['email'] = request.form.get('email')
+        meta_user['name'] = request.form.get('name')
+        conn = sqlite3.connect('child.sqlite')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users where email='%s'" % meta_user['email'])
+        if c.fetchone():
+            error_message = "user_exists"
+        else:
+            c.execute("INSERT INTO users "
+                      "(email, name) "
+                      "VALUES "
+                      "('{email}','{name}')"
+                      "".format(**meta_user))
+            conn.commit()
+            user_created = True
+        conn.commit()
+        conn.close()
+        return redirect('/Success')
+    return render_template('add_user.html',
+                           user_created = user_created,
+                           error_message = error_message)
+
 
 @app_child.route('/Success', methods=['GET', 'POST'])
 def success():
